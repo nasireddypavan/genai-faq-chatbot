ed567cdba389cbd262afa48b1a953243fb94a41c
nasireddypavan <66197691+nasireddypavan@users.noreply.github.com>
2025-05-04 15:20:00 +0000

    small change in .sh

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 gunicorn -w 4 -k uvicorn.workers.UvicornWorker app.main:app --bind 0.0.0.0:8000 &  
 
 # Start Streamlit  
-streamlit run app/streamlit_app.py --server.port 8501 --server.enableCORS false  
+streamlit run streamlit_ui.py --server.port 8501 --server.enableCORS false  
